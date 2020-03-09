b2f7436052bc88f443c3d4c46a76e85a4f19fb37
eduardo82926 <eduardo82926@gmail.com>
2020-03-09 11:05:24 -0300

    agrupamento kmeans

--- Teste1.py
+++ Teste1.py
@@ -1,27 +1,35 @@
+from pandas import DataFrame
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
 
 class Teste1:
 
     def __init__(self):
         self.training_file = 'bic-training/BIC.txt'
-        self.samples = []
 
-    def main(self):
-        self.samples = self.get_samples()
-        print(self.samples)
+    def main(self):)
+        self.amostras = np.array(self.get_samples())
+        print(a.shape)
+        modelo = KMeans(n_clusters=4)
+        modelo.fit(self.amostras)
+        print('Grupos {}' .format(modelo.labels_))
+
 
     def get_samples(self):
-        samples = []
-        lineList = [line.rstrip('\n') for line in open(self.training_file)]
-        for line in lineList:
-            samples.append(line.split(' '))
-
-        for sample in samples:
-            sample.pop()
-            sample.pop()
-            sample.pop(0)
-            sample.pop(0)
-        
-        return samples
+        linhas = [linha.rstrip('\n') for linha in open(self.training_file)]
+        aux = []
+        for linha in linhas[1:]: #skip primeira linha
+            auxList = linha.split(' ')
+            auxList.pop()
+            auxList.pop()
+            auxList.pop(0)
+            auxList.pop(0)
+            auxList = list(map(float, auxList))
+            auxList = np.array(auxList)
+            aux.append(auxList)
+        return aux
 
 
 if __name__ == '__main__':
